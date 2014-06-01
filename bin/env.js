16917d7f73bb802f398b8f5c63ae84bc9bd76e42
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-06-02 00:49:50 +0200

    add specific css for wp8
    web fonts don't work in wp ie10 webview

--- bin/env.js
+++ bin/env.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
 var preprocess = require('preprocess');
+var argv = process.argv.slice(2);
 
 preprocess.preprocessFileSync(
     './env/index.tmpl',
     './env/index.js',
     {
-        TARGET: process.argv.slice(2)[0]
+        TARGET: argv[0]
     }
 );
--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
 var preprocess = require('preprocess');
+var argv = process.argv.slice(2);
 
 preprocess.preprocessFileSync(
     './www/index.tmpl',
     './www/index.html', 
     {
-        ZANIMO_ENV: process.argv.slice(2)[0]
+        ZANIMO_ENV: argv[0],
+        IS_WP8: argv[1] ? true : false
     }
 );
